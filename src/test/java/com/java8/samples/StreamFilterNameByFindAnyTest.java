c15266715ab41bd8013ef91da863f4326e5eee92
jpmani <jmanikandan.autolib@gmail.com>
2020-08-30 20:20:39 +0530

    Test and Resrc packages included

---
+++ src/test/java/com/java8/samples/StreamFilterNameByFindAnyTest.java
@@ -0,0 +1,14 @@
+package com.java8.samples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StreamFilterNameByFindAnyTest {
+
+	@Test
+	public void test() {
+		System.out.println("Junit Test Class:");
+	}
+
+}
